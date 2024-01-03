5636df5329f31e5f494064b74e9c9de4ad1f69b2
Niels Gregers Johansen <niels@jumpto365.com>
2024-01-03 17:09:33 +0100

    baseline

---
+++ projects/cli/cmd/root.go
@@ -0,0 +1,60 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+
+// https://ascii-generator.site/t/ banner3
+var rootCmd = &cobra.Command{
+	Use:   "cava",
+	Short: "A brief description of your application",
+	Long: "\033[33m" + `
+
+
+
+ ######     ###    ##     ##    ###    
+##    ##   ## ##   ##     ##   ## ##   
+##        ##   ##  ##     ##  ##   ##  
+##       ##     ## ##     ## ##     ## 
+##       #########  ##   ##  ######### 
+##    ## ##     ##   ## ##   ##     ## 
+ ######  ##     ##    ###    ##     ## 
+
+										
+	
+` +
+		"\033[0m" + `
+CAVA ...
+                                                               
+
+	
+	`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cava.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ projects/cli/config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"context"
+	"log"
+	"strings"
+
+	"github.com/kamva/mgm/v3"
+	"github.com/spf13/viper"
+	"go.mongodb.org/mongo-driver/event"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func MongoConnectionString() string {
+	databaseUrl := strings.ReplaceAll(viper.GetString("DATABASEURL"), "mongodb://", "")
+	return "mongodb://" + viper.GetString("DATABASEADMIN") + ":" + viper.GetString("DATABASEPASSWORD") + "@" + databaseUrl
+}
+
+func DatabaseName() string {
+	return viper.GetString("DATABASE")
+}
+func Setup(envPath string) {
+	viper.SetConfigFile(envPath)
+	viper.AutomaticEnv()
+	viper.ReadInConfig()
+
+	cmdMonitor := &event.CommandMonitor{
+		Started: func(_ context.Context, evt *event.CommandStartedEvent) {
+			// log.Print(evt.Command)
+		},
+	}
+
+	if viper.GetString("DATABASEADMIN") == "" {
+
+		return
+	}
+	db := DatabaseName()
+	err := mgm.SetDefaultConfig(nil, db, options.Client().ApplyURI(MongoConnectionString()).SetMonitor(cmdMonitor))
+	if err != nil {
+		log.Println(err)
+	}
+
+}
